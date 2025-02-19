573bf5c4fe1d018cbcbd68c6004aeb56c252d92a
yzxx517 <zaq52015201@gmail.com>
2025-02-19 11:56:00 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,15 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-s存檔成 week01-1.cpp要記得家 .cpp
+/// 下週考試考這題,不用寫藍色的
+#include <stdio.h> ///檔名不能寫錯
+///studio的意思是 標準的standard input/output
+///有了 stdio.h的外掛,才能用 scanf() printf()
+int main()
+{
+    int a, b; /// 整數 a, b
+    scanf( "%d" , &a ); ///正課剛剛教, 掃描讀東西
+    scanf( "%d" , &b ); ///可以用複製的, 再改b
+
+    printf("%d", a+b ); ///實習課有教, 印東西
+}
