6320fab4055f844942c636c6a93bde466f008c76
yzxx517 <zaq52015201@gmail.com>
2025-02-26 12:03:27 +0800

    week02

---
+++ week02-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 123, b = 100;
+    printf("°£ªk %d ", a / b);
+    printf("¾l¼Æ %d ", a % b);
+
+}
---
+++ week02-3.py
@@ -0,0 +1,15 @@
+#week02-03.py 寫LeetCode
+#LeetCode 1. Two Sum
+#有一堆數字 nums 裡面哪兩個相加,是target
+#nums[i] + nums[j] == target 找到 i 和 j 使得加起來是 target
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        box = {} # 有一個箱子,裡面放出現過的數字
+        # 我們想要湊出 target 這個加總
+
+        for i,num in enumerate(nums):
+            other = target- num #　另外一個重要的數可以湊出taget的(target-num)
+            if other in box: #　在箱子裡,有另外一個重要的數
+                return [ box[other], i] #　找到答案
+            else: # 如果沒有合過的數字能湊
+                box[num] = i #就把現在的數字 num , 放到 box 裡面
